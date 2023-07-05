6b385cbbc516c276d666683c23469719cb2d10ac
withme <84191029+seung-mii@users.noreply.github.com>
2023-07-05 20:19:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 876 ms, Memory: 196212 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,50 +1,26 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int N = s.nextInt();
-        int[] ary = new int[N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] counting = new int[20000001];
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for(int i = 0; i < N; i++) {
-            ary[i] = s.nextInt();
+        for(int i = 0 ; i < N; i++) {
+            counting[Integer.parseInt(st.nextToken()) + 10000000]++;
         }
 
-        Arrays.sort(ary);
-
-        int M = s.nextInt();
-        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
 
         for(int i = 0; i < M; i++) {
-            int num = s.nextInt();
-            sb.append(upperBound(ary, num) - lowerBound(ary, num)).append(' ');
+            sb.append(counting[Integer.parseInt(st.nextToken()) + 10000000]).append(' ');
         }
+        
         System.out.println(sb);
-    }
-
-    private static int lowerBound(int[] ary, int num) {
-        int start = 0;
-        int end = ary.length;
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            if (num <= ary[mid]) end = mid;
-            else start = mid + 1;
-        }
-        return start;
-    }
-
-    private static int upperBound(int[] ary, int num) {
-        int start = 0;
-        int end = ary.length;
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            if (num < ary[mid]) end = mid;
-            else start = mid + 1;
-        }
-        return start;
     }
 }
