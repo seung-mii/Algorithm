cf05efb1831428799ce545dfd37fc797065d4940
withme <84191029+seung-mii@users.noreply.github.com>
2023-07-08 02:52:11 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,16 +1,20 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int answer = 0;
-        int L = s.nextInt();
-        String str = s.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        long result = 0;
+        long pow = 1;
 
-        for (int i = 0; i < L; i++) {
-            answer += (int)((str.charAt(i) - 96) * (Math.pow(31, i)));
+        for(int i = 0; i < L; i++) {
+            result += ((S.charAt(i) - 96) * pow);
+            // pow는 31을 매번 곱함
+            pow = (pow * 31) % 1234567891;
+            // 곱해줄 때마다 1234567891을 나눠주면 long을 넘지 않음
         }
-
-        System.out.println(answer);
+        
+        System.out.println(result % 1234567891);
     }
 }
