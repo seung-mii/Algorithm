14af5636c52d0741a927b71d03eebaa3ccee3fb7
withme <84191029+seung-mii@users.noreply.github.com>
2024-07-13 23:29:54 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,13 +1,9 @@
 t = int(input())
+money = [25, 10, 5, 1]
 
-for i in range(t) :
-  q, d, n, p = 0, 0, 0, 0
-  money = int(input())
-  q += money // 25
-  money %= 25
-  d += money // 10
-  money %= 10
-  n += money // 5
-  money %= 5
-  p += money // 1
-  print(q,d,n,p,sep=' ')
+for i in range(t):
+  c = int(input())
+  for j in range(4):
+    print(c // money[j], end=" ")
+    c %= money[j]
+  print()
