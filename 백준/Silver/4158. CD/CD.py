6d31ddf13f593f91bdb474ad1e0f7a619ef80046
withme <84191029+seung-mii@users.noreply.github.com>
2024-07-31 02:05:41 +0900

    [Silver V] Title: CD, Time: 1804 ms, Memory: 255896 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.py
+++ 백준/Silver/4158. CD/CD.py
@@ -3,24 +3,10 @@ input = sys.stdin.readline
 
 while True:
   n, m = map(int, input().split())
+  n_number = {int(input()) for _ in range(n)}
+  m_number = {int(input()) for _ in range(m)}
+
   if n == 0 and m == 0:
     break
-
-  n_number = [int(input()) for _ in range(n)]
-  m_number = [int(input()) for _ in range(m)]
-  cd = 0
-
-  for i in range(m):
-    start, end = 0,  n-1
-
-    while start <= end:
-      mid = (start+end) // 2
-
-      if n_number[mid] == m_number[i]:
-        cd += 1
-        break
-      elif n_number[mid] > m_number[i]:
-        end = mid - 1
-      elif n_number[mid] < m_number[i]:
-        start = mid + 1
-  print (cd)
+  
+  print(len(n_number & m_number)) # 교집합
