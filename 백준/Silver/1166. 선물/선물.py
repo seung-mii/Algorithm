6d9dd9201fabfd4e9bb62e6a50279f24995118e5
withme <84191029+seung-mii@users.noreply.github.com>
2024-07-31 02:56:03 +0900

    [Silver III] Title: 선물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1166. 선물/선물.py
@@ -0,0 +1,12 @@
+n, l, w, h = map(int, input().split())
+
+start, end = 0, max(l, w, h)
+for i in range(100):
+    mid = (start + end) / 2
+    
+    if (l // mid) * (w // mid) * (h // mid) >= n:
+      start = mid
+    else:
+      end = mid
+
+print(start)
