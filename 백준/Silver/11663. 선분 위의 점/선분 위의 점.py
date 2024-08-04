b0dc2dacad5b680deb23fac89b607541bad443d6
withme <84191029+seung-mii@users.noreply.github.com>
2024-08-04 20:35:52 +0900

    [Silver III] Title: 선분 위의 점, Time: 692 ms, Memory: 42532 KB -BaekjoonHub

---
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+point = sorted(list(map(int, input().split())))
+
+def bs(ary, l, switch):
+  start, end = 0, n-1
+  i = -1
+
+  while start <= end:
+    mid = (start + end) // 2
+
+    if ary[mid] < l:
+      i = mid
+      start = mid + 1
+    elif ary[mid] == l:
+      i = (mid - 1 if switch == 1 else mid)
+      start = mid + 1
+    else:
+      end = mid - 1
+
+  return i
+
+for i in range(m):
+  l1, l2 = map(int, input().split())
+  i1, i2 = 0, 0
+  i1 = bs(point, l1, 1)
+  i2 = bs(point, l2, 2)
+  print(i2 - i1)
