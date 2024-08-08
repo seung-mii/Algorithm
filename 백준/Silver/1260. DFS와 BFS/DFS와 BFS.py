302b0556bb9bd334c4da47cf0de6fba1fcfe94b9
withme <84191029+seung-mii@users.noreply.github.com>
2024-08-09 00:52:14 +0900

    [Silver II] Title: DFS와 BFS, Time: 544 ms, Memory: 39596 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,37 +1,36 @@
 from collections import deque
 
 n, m, v = map(int, input().split())
+g = [[False] * (n+1) for i in range(n+1)]
 
-graph = [[False] * (n + 1) for _ in range(n + 1)]
+for i in range(m):
+  x, y = map(int, input().split())
+  g[x][y] = True
+  g[y][x] = True
 
-for _ in range(m):
-  a, b = map(int, input().split())
-  graph[a][b] = True
-  graph[b][a] = True
-
-visited1 = [False] * (n + 1)
-visited2 = [False] * (n + 1)
+visit1 = [False] * (n+1)
+visit2 = [False] * (n+1)
 
 def dfs(v):
-  visited1[v] = True
-  print(v, end=" ")
+  visit1[v] = True
+  print(v, end=' ')
 
-  for i in range(1, n + 1):
-    if not visited1[i] and graph[v][i]:
+  for i in range(1, n+1):
+    if visit1[i] == False and g[v][i] == True:
       dfs(i)
 
 def bfs(v):
   q = deque([v])
-  visited2[v] = True
+  visit2[v] = True
 
   while q:
     v = q.popleft()
-    print(v, end=" ")
+    print(v, end=' ')
 
-    for i in range(1, n + 1):
-      if not visited2[i] and graph[v][i]:
+    for i in range(1, n+1):
+      if visit2[i] == False and g[v][i] == True:
         q.append(i)
-        visited2[i] = True
+        visit2[i] = True
 
 dfs(v)
 print()
