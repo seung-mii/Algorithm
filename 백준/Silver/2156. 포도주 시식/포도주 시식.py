1b05107f5a441093cede700b633ed6a4c8da1011
withme <84191029+seung-mii@users.noreply.github.com>
2024-09-13 00:06:32 +0900

    [Silver I] Title: 포도주 시식, Time: 272 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,15 +1,15 @@
-n = int(input()) 
-grape = [0] * (n+1)
+n = int(input())
+grape = [0] 
+for _ in range(n):
+  grape.append(int(input()))
 
-for i in range(1, n+1):
-  grape[i] = int(input()) 
+dp = [0] * (n+1)
+dp[1] = grape[1]
 
-dp = [0, grape[1]]
+if n > 1: 
+  dp[2] = grape[1] + grape[2]
 
-if n > 1:
-  dp.append(grape[1] + grape[2])
+for i in range(3, n+1):
+  dp[i] = max(grape[i] + dp[i-2], grape[i] + grape[i-1] + dp[i-3], dp[i-1]) 
 
-for i in range(3, n+1): 
-  dp.append(max(dp[i-1], grape[i] + grape[i-1] + dp[i-3], grape[i] + dp[i-2]))
-
-print(max(dp))
+print(dp[n])
