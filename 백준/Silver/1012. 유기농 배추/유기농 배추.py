f538be66173d7e7de5ae7863fabd199d231f7259
withme <84191029+seung-mii@users.noreply.github.com>
2024-10-05 05:42:13 +0900

    [Silver II] Title: 유기농 배추, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,35 +1,40 @@
-for _ in range(int(input())):
-  m, n, k = map(int, input().split())
+from collections import deque
+import sys
+input = sys.stdin.readline
 
-  d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-  graph = [[0] * (n) for _ in range(m)]
-  visited = [[0] * (n) for _ in range(m)]
-  cnt = 0
+t = int(input())
+
+for _ in range(t):
+  m, n, k = map(int, input().split())
+  d = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+  graph = [[0] * n for _ in range(m)]
+  visited = [[-1] * n for _ in range(m)]
+  num = 0
 
   for __ in range(k):
     x, y = map(int, input().split())
-    graph[x][y] = 1
-    visited[x][y] = -1
-    
+    graph[x][y] = 1   # 배추 1, 안배추 0
+    visited[x][y] = 0 # 방문하면안됨 -1, 방문안함 0, 방문함 1
+
   def dfs(x, y):
-    stack = [(x, y)]
+    q = deque([(x, y)])
     visited[x][y] = 1
 
-    while stack:
-      x, y = stack.pop()
+    while q:
+      x, y = q.popleft()
 
       for i in range(4):
-        nx = x + d[i][0]
-        ny = y + d[i][1]
+        dx = x + d[i][0]
+        dy = y + d[i][1]
 
-        if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 1 and visited[nx][ny] == -1:
-          stack.append((nx, ny))
-          visited[nx][ny] = 1
-  
-  for x in range(m):
-    for y in range(n):
-      if visited[x][y] == -1:
-        cnt += 1
-        dfs(x, y)
+        if 0 <= dx < m and 0 <= dy < n and visited[dx][dy] == 0 and graph[dx][dy] == 1:
+          visited[dx][dy] = 1
+          q.append((dx, dy))
 
-  print(cnt)
+  for i in range(m):
+    for j in range(n):
+      if visited[i][j] == 0 and graph[i][j] == 1:
+        num += 1
+        dfs(i, j)
+
+  print(num)
