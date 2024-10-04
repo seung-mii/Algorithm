c118c4ec2eac2676a8019642e46d64b9e52e7026
withme <84191029+seung-mii@users.noreply.github.com>
2024-10-05 08:04:30 +0900

    [Silver I] Title: 컴백홈, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.py
+++ 백준/Silver/1189. 컴백홈/컴백홈.py
@@ -3,29 +3,36 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 r, c, k = map(int, input().split())
-graph = [list(input().strip()) for _ in range(r)] 
-visited = [[0] * c for _ in range(r)] 
-d = [(1, 0), (0, 1), (0, -1), (-1, 0)] 
-num = 0 
+graph = [[0] * c for _ in range(r)]  
+visited = [[0] * c for _ in range(r)]  
+d = [(1, 0), (0, 1), (0, -1), (-1, 0)]  
+num = 0
+
+for i in range(r):
+  s = list(input().strip())  
+  for j in range(c):
+    if s[j] == 'T':  
+      graph[i][j] = -1  
+
 
 def dfs(x, y, dis):
   global num
-
-  if x == 0 and y == c-1:
-    if dis == k:
+  
+  if x == 0 and y == c - 1:
+    if dis == k:  
       num += 1
     return
-
+    
   visited[x][y] = 1
-
+  
   for i in range(4):
     dx = x + d[i][0]
     dy = y + d[i][1]
-     
-    if 0 <= dx < r and 0 <= dy < c and not visited[dx][dy] and graph[dx][dy] != 'T':
-      dfs(dx, dy, dis + 1)
-   
-  visited[x][y] = 0
 
-dfs(r - 1, 0, 1) 
-print(num) 
+    if 0 <= dx < r and 0 <= dy < c and visited[dx][dy] == 0 and graph[dx][dy] != -1:
+      dfs(dx, dy, dis + 1)
+
+  visited[x][y] = 0  
+
+dfs(r - 1, 0, 1)
+print(num)
