016e54284830d3e07c1d2476be74c6c609f31b38
withme <84191029+seung-mii@users.noreply.github.com>
2024-10-19 06:00:28 +0900

    [Gold IV] Title: 연구소, Time: 2292 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -3,45 +3,44 @@ import copy
 from collections import deque
 from itertools import combinations
 
-n, m = map(int, input().split()) 
-d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-max_count = 0
+d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 empty = []
 virus = []
+max_count = 0
 
 for i in range(n):
   for j in range(m):
     if graph[i][j] == 0:
       empty.append((i, j))
-    elif graph[i][j] == 2:
+    if graph[i][j] == 2:
       virus.append((i, j))
 
-def bfs(temp_graph, temp_virus):
-  q = deque(temp_virus)
+def dfs(graph, virus):
+  q = deque(virus)
 
   while q:
     x, y = q.popleft()
-    
+
     for i in range(4):
       dx = x + d[i][0]
       dy = y + d[i][1]
-      
-      if 0 <= dx < n and 0 <= dy < m and temp_graph[dx][dy] == 0:
-        temp_graph[dx][dy] = 2
+
+      if 0 <= dx < n and 0 <= dy < m and graph[dx][dy] == 0:
+        graph[dx][dy] = 2
         q.append((dx, dy))
 
-def safe(temp_graph):
-  count = sum(row.count(0) for row in temp_graph)
+def safe(graph):
+  count = sum(row.count(0) for row in graph)
   return count
-  
+
 for walls in combinations(empty, 3):
   temp_graph = copy.deepcopy(graph)
   for x, y in walls:
     temp_graph[x][y] = 1
 
-  bfs(temp_graph, virus)
-
+  dfs(temp_graph, virus)
   count = safe(temp_graph)
   max_count = max(count, max_count)
 
