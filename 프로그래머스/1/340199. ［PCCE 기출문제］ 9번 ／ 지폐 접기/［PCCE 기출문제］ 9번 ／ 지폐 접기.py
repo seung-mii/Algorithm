a467fb36660a95b507f4dffa97d9a104e9351c13
withme <84191029+seung-mii@users.noreply.github.com>
2024-10-26 15:01:13 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -1,11 +1,8 @@
 def solution(wallet, bill):
     answer = 0
     
-    while True:
-        if max(bill) > max(wallet) or min(bill) > min(wallet):
-            bill[bill.index(max(bill))] = int(bill[bill.index(max(bill))] / 2)
-            answer += 1
-        elif max(bill) <= max(wallet) and min(bill) <= min(wallet):
-            break
+    while max(bill) > max(wallet) or min(bill) > min(wallet):
+        bill[bill.index(max(bill))] = int(bill[bill.index(max(bill))] / 2)
+        answer += 1
     
     return answer
