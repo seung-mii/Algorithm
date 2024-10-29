c412a8036a8a5cf0c22bd9b8c819f6eaa2d4cc5c
withme <84191029+seung-mii@users.noreply.github.com>
2024-10-29 17:24:41 +0900

    [level 1] Title: 햄버거 만들기, Time: 340.16 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -3,14 +3,11 @@ def solution(ingredient):
     stack = []
 
     for i in ingredient:
-        if i != 1 and len(stack) == 0:
-            continue
-        else: 
-            stack.append(i)
+        stack.append(i)
 
-            if stack[-4:] == [1, 2, 3, 1]:
-                answer += 1 
-                for _ in range(4):
-                    stack.pop()
+        if stack[-4:] == [1, 2, 3, 1]:
+            answer += 1 
+            for _ in range(4):
+                stack.pop()
 
     return answer
