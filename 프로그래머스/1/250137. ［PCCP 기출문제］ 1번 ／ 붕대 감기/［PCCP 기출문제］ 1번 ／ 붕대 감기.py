182b1585f52cd844d49b6cae6b00592691e4e4a4
withme <84191029+seung-mii@users.noreply.github.com>
2024-10-31 01:09:29 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.65 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -0,0 +1,31 @@
+def solution(bandage, health, attacks):
+    cur_health = health
+    count = 0
+    
+    for i in range(1, attacks[len(attacks)-1][0]+1):
+        # 공격 여부
+        if i != attacks[0][0]:
+            count += 1 # 1
+            
+            # 체력 회복
+            cur_health += bandage[1] # 5
+            if cur_health > health:
+                cur_health = health
+                
+            # 연속 성공
+            if count == bandage[0]:
+                cur_health += bandage[2]
+                if cur_health > health:
+                    cur_health = health
+                count = 0
+        else:
+            cur_health -= attacks[0][1]
+            count = 0
+            attacks.pop(0)
+        
+        # 사망
+        if cur_health <= 0:
+            cur_health = -1
+            break
+            
+    return cur_health
