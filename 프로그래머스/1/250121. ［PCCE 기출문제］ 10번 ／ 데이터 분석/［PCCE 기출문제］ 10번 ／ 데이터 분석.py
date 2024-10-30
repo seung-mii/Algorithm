d5fc1eeb2ab4b37460a23973489cdd0ad9e28210
withme <84191029+seung-mii@users.noreply.github.com>
2024-10-30 21:44:55 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.33 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,27 +1,9 @@
 def solution(data, ext, val_ext, sort_by):
     answer = []
-    
-    for d in data:
-        if ext == 'code': 
-            if d[0] < val_ext:
-                answer.append(d)
-        elif ext == 'date':
-            if d[1] < val_ext:
-                answer.append(d)
-        elif ext == 'maximum':
-            if d[2] < val_ext:
-                answer.append(d)
-        elif ext == 'remain':
-            if d[3] < val_ext:
-                answer.append(d)
-    
-    if sort_by == 'code': 
-        answer.sort(key=lambda x: x[0])
-    elif sort_by == 'date':
-        answer.sort(key=lambda x: x[1])
-    elif sort_by == 'maximum':
-        answer.sort(key=lambda x: x[2])
-    elif sort_by == 'remain':
-        answer.sort(key=lambda x: x[3])
+    by = [ "code", "date", "maximum", "remain" ]
 
-    return answer
+    for d in data:
+        if d[by.index(ext)] < val_ext:
+            answer.append(d)
+
+    return sorted(answer, key=lambda x: x[by.index(sort_by)])
