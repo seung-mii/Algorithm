b38116bcb591476b1e822ec6a122034e06953bb1
withme <84191029+seung-mii@users.noreply.github.com>
2024-11-05 16:02:14 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,6 +1,3 @@
 def solution(s):
-    answer = ''
     ary = list(map(int, s.split()))
-    answer += str(min(ary)) + ' '
-    answer += str(max(ary))
-    return answer
+    return str(min(ary)) + ' ' + str(max(ary))
