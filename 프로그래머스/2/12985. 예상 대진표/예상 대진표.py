869ca44781d508076e8a81af74a0c691a761c4e8
withme <84191029+seung-mii@users.noreply.github.com>
2024-11-18 17:14:59 +0900

    [level 2] Title: 예상 대진표, Time: 656.21 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,6 +1,5 @@
 def solution(n,a,b):
     answer = 0
-    flag = False
     a -= 1
     b -= 1
     ary = []
@@ -8,17 +7,14 @@ def solution(n,a,b):
     for i in range(n):
         ary.append(i) 
     
-    while flag == False:
+    while True:
         temp = []
         answer += 1
         for i in range(0, len(ary), 2): 
-            if ary[i] != a and ary[i+1] != a and ary[i] != b and ary[i+1] != b:
-                temp.append(ary[i])
-            if ary[i] == a or ary[i] == b:
+            if (ary[i] != a and ary[i+1] != a and ary[i] != b and ary[i+1] != b) or (ary[i] == a or ary[i] == b):
                 temp.append(ary[i])
             if ary[i+1] == a or ary[i+1] == b:
                 temp.append(ary[i+1])
             if (ary[i] == a and ary[i+1] == b) or (ary[i] == b and ary[i+1] == a):
-                flag = True
+                return answer
         ary = temp
-    return answer
