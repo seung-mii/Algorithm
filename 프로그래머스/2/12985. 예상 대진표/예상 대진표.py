5bf91350e7ed8969be170e888c7a1dc9a7ce9417
withme <84191029+seung-mii@users.noreply.github.com>
2024-11-18 17:12:23 +0900

    [level 2] Title: 예상 대진표, Time: 842.47 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,24 @@
+def solution(n,a,b):
+    answer = 0
+    flag = False
+    a -= 1
+    b -= 1
+    ary = []
+    
+    for i in range(n):
+        ary.append(i) 
+    
+    while flag == False:
+        temp = []
+        answer += 1
+        for i in range(0, len(ary), 2): 
+            if ary[i] != a and ary[i+1] != a and ary[i] != b and ary[i+1] != b:
+                temp.append(ary[i])
+            if ary[i] == a or ary[i] == b:
+                temp.append(ary[i])
+            if ary[i+1] == a or ary[i+1] == b:
+                temp.append(ary[i+1])
+            if (ary[i] == a and ary[i+1] == b) or (ary[i] == b and ary[i+1] == a):
+                flag = True
+        ary = temp
+    return answer
