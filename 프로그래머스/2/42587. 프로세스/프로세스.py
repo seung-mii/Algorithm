d991f4f27e191d19bb8462e53f431bee61f45eaa
withme <84191029+seung-mii@users.noreply.github.com>
2024-12-09 10:29:56 +0900

    [level 2] Title: 프로세스, Time: 1.75 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 def solution(priorities, location):
     cnt = 1
     dic = {}
@@ -9,13 +7,10 @@ def solution(priorities, location):
 
     while cnt != len(dic) - 1:
         for d in dic:
-            if dic[d] == -1:
-                continue
-            else:
-                if max(dic.values()) == dic[d]:
-                    if d == location:
-                        return cnt
-                    else:
-                        dic[d] = -1
-                        cnt += 1
+            if max(dic.values()) == dic[d]:
+                if d == location:
+                    return cnt
+                else:
+                    dic[d] = -1
+                    cnt += 1
     return cnt
